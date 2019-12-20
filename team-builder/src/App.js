0bf0706d2d21abf02c1601e37514941604d994e0
skooger <adamjskooglambda@gmail.com>
2019-12-20 07:58:23 -0800

    updating from local and completed MVP

---
+++ team-builder/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TeamForm from './Components/TeamForm';
+
+function App() {
+  return (
+    <div className="App">
+      <TeamForm/> 
+    </div>
+  );
+}
+
+export default App;
